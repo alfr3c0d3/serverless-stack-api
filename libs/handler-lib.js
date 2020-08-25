0bc56b2fbab938255824406cdcc4a5733d3b6f46
Alfredo Morales <alfr3dit0.dev@gmail.com>
2020-08-24 20:18:11 -0400

    Created CRUD

---
+++ libs/handler-lib.js
@@ -0,0 +1,26 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      // Return the error
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+  };
+}
